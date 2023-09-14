960f2044d8d19eafe56bfd6b97bcda3b80ab2ca0
Pedro Couto <pedrocouto152001@gmail.com>
2023-09-14 09:46:42 -0300

    Geração de erros personalizados

--- src/main/java/com/example/trabalho_pratico/controllers/homeController.java
+++ src/main/java/com/example/trabalho_pratico/controllers/homeController.java
@@ -2,9 +2,13 @@ package com.example.trabalho_pratico.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Controller
 public class homeController {
@@ -18,6 +22,23 @@ public class homeController {
             return "menu";
         }
         return "redirect:/login";
+    }
+
+    @GetMapping("/error404")
+    public void handle404(HttpServletRequest request) throws NoHandlerFoundException {
+        throw new NoHandlerFoundException("GET", request.getRequestURI(), null);
+    }
+
+    @GetMapping("/errorJava")
+    public String handleJava(Model model) throws NoHandlerFoundException {
+            try {
+                int result = 10 / 0;
+            } catch (ArithmeticException e) {
+                model.addAttribute("errorMessage", e.getMessage());
+                model.addAttribute("errorCause", e.getCause());
+                return "error/template_error";
+            }
+        return "menu";
     }
 
 }

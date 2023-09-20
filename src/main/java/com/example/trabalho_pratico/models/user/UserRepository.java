7c98c1c980ad6f89e4371945cd25e61e1449719b
Pedro Couto <pedrocouto152001@gmail.com>
2023-09-19 21:45:45 -0300

    Refatorando UserRepository

--- src/main/java/com/example/trabalho_pratico/models/user/UserRepository.java
+++ src/main/java/com/example/trabalho_pratico/models/user/UserRepository.java
@@ -2,8 +2,4 @@ package com.example.trabalho_pratico.models.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    public static User login() {
-        return null;
-    }
-}
+public interface UserRepository extends JpaRepository<User, Long> {}
